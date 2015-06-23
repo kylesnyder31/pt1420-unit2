791f2d03a3242b41e23ce1f71924d10e2172f9ea
kylesnyder31 <kylesnyder31@gmail.com>
2015-06-22 21:38:17 -0500

    working form alert

--- application.js
+++ application.js
@@ -1,1 +1,9 @@
-alert("Hello world");
+// alert("Hello world");
+function processForm(){
+	var name;
+	var message;
+	name = document.getElementById("name");
+    message = document.getElementById("message");
+	
+	alert("Hello " + name.value + " your message is: " + message.value);
+}
