f2c18a0470bfa1fc800bb26724e62df69d6571c3
kylesnyder31 <kylesnyder31@gmail.com>
2015-06-22 22:37:23 -0500

    save me now

--- application.js
+++ application.js
@@ -2,8 +2,12 @@
 function processForm(){
 	var name;
 	var message;
+	var content
 	name = document.getElementById("name");
     message = document.getElementById("message");
+	content = document.getElementById("output")
 	
-	alert("Hello " + name.value + " your message is: " + message.value);
+	//alert("Hello " + name.value + " your message is: " + message.value);
+	
+	content.innerText = "Hello " + name.value + "your message is: " + message.value;
 }
